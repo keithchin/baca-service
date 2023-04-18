cc14154602cf27edd1e4b2f2bc1d89a185006d90
keithchin <kcisbug@gmail.com>
2023-04-18 21:36:09 +0800

    (Feat.) Added auth methods into UserService

--- src/services/User/UserService.ts
+++ src/services/User/UserService.ts
@@ -1,4 +1,7 @@
 import { Model, ObjectId } from 'mongoose'
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
 import { IUser } from '../../interfaces/User/IUser';
 import { IUserService } from '../../interfaces/User/IUserService';
 import userModel from './UserModel';
@@ -8,6 +11,22 @@ const UserModel: Model<IUser> = userModel;
 
 
 export class UserService implements IUserService {
+
+  private async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+  }
+
+  private async verifyPassword(password: string, hashedPassword: string): Promise<boolean> {
+    return await bcrypt.compare(password, hashedPassword);
+  }
+
+  private signJwt(userId: ObjectId): string {
+    const secretKey = process.env.JWT_SECRET || 'bluebanana';
+    return jwt.sign({ userId }, secretKey, { expiresIn: '1h' });
+  }
+
   public async getAllUsers(): Promise<IUser[]> {
       console.log('hi');
       try {
@@ -40,15 +59,32 @@ export class UserService implements IUserService {
     }
   
     public async createUser(createUserDto: CreateUpdateUserDto): Promise<IUser> {
-        try {
-          const user = new UserModel(createUserDto);
-          await user.save();
-          return user;
-        } catch (error) {
-          throw new Error((error as Error).message);
-        }
+      try {
+        const hashedPassword = await this.hashPassword(createUserDto.password);
+        const user = new UserModel({ ...createUserDto, password: hashedPassword });
+        await user.save();
+        return user;
+      } catch (error) {
+        throw new Error((error as Error).message);
       }
-  
+    }
+
+    public async authenticateUser(usernameOrEmail: string, password: string): Promise<string | null> {
+      const user = await this.getUserByUsernameOrEmail(usernameOrEmail);
+    
+      if (!user) {
+        return null;
+      }
+    
+      const isPasswordValid = await this.verifyPassword(password, user.password);
+    
+      if (!isPasswordValid) {
+        return null;
+      }
+    
+      return this.signJwt(user._id);
+    }
+    
     public async updateUserById(userId: ObjectId, updateUserDto: CreateUpdateUserDto): Promise<IUser | null> {
       try {
         const user = await UserModel.findByIdAndUpdate(userId, updateUserDto, { new: true });
